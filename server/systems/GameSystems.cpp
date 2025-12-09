9075c4ed6275d0719cef0ce0806510a2626a51b8
hugopoggetti <hugo.poggetti@epitech.eu>
2025-12-09 17:30:53 +0100

    [Feat] first impl of game systems functions

--- server/systems/GameSystems.cpp
+++ server/systems/GameSystems.cpp
@@ -1,5 +1,51 @@
 #include "GameSystems.hpp"
+#include "server/components/Position.hpp"
+#include "server/components/EntityType.hpp"
+#include "server/components/Motion.hpp"
+#include <iostream>
 
-void Systems::movement_system() {}
-void Systems::collision_system() {}
-void Systems::cleanup_system() {}
+auto Systems::movement_system(
+    [[maybe_unused]] Registry &r,
+    std::unordered_map<std::size_t, std::tuple<Position, Motion, EntityTag>>
+        items) -> void
+{
+    for (auto &&[i, item] : items) {
+        EntityTag &tag = std::get<EntityTag>(item);
+        Position &position = std::get<Position>(item);
+        Motion &motion = std::get<Motion>(item);
+        if (tag.type == EntityType::PLAYER) {
+            // special case for user input
+            std::cout << "player\n";
+        } else {
+            std::cout << "other\n";
+            position.x += motion.velocity_x;
+            position.y += motion.velocity_y;
+            motion.velocity_x += motion.acceleration_x;
+            motion.velocity_y += motion.acceleration_y;
+        }
+    }
+}
+
+auto Systems::collision_system(
+    [[maybe_unused]] Registry &r,
+    std::unordered_map<std::size_t, std::tuple<Collision, EntityTag>> items)
+    -> void
+{
+    // check collision and sub health to player and boss
+    for (auto &&[i, item] : items) {
+        [[maybe_unused]] EntityTag &tag = std::get<EntityTag>(item);
+    }
+}
+
+auto Systems::cleanup_system(
+    [[maybe_unused]] Registry &r,
+    std::unordered_map<std::size_t, std::tuple<Position, EntityTag>> items)
+    -> void
+{
+    // check if it's a bullet and not on window anymore
+    for (auto &&[i, item] : items) {
+        [[maybe_unused]] EntityTag &tag = std::get<EntityTag>(item);
+        // if not on window:
+        // r.kill_entity(r.entity_from_index(i));
+    }
+}
--- server/systems/GameSystems.hpp
+++ server/systems/GameSystems.hpp
@@ -8,10 +8,32 @@
 #ifndef COMP_POSITION_HPP
 #define COMP_POSITION_HPP
 
-namespace Systems {
-    void movement_system();
-    void collision_system();
-    void cleanup_system();
-}
+#include "../../ecs/regisrty/Registry.hpp"
+#include "../components/Collision.hpp"
+#include "../components/EntityType.hpp"
+#include "../components/Health.hpp"
+#include "../components/Laser.hpp"
+#include "../components/Motion.hpp"
+#include "../components/Position.hpp"
+
+namespace Systems
+{
+
+auto movement_system(
+    Registry &r,
+    std::unordered_map<std::size_t, std::tuple<Position, Motion, EntityTag>>
+        items) -> void;
+// bullets collision between boss and players
+auto collision_system(
+    Registry &r,
+    std::unordered_map<std::size_t, std::tuple<Collision, EntityTag>> items)
+    -> void;
+
+// just remove boss bullets when not on screen anymore
+auto cleanup_system(
+    Registry &r,
+    std::unordered_map<std::size_t, std::tuple<Position, EntityTag>> items)
+    -> void;
+} // namespace Systems
 
 #endif /* COMP_POSITION_HPP */
