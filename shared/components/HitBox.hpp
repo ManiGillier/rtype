25c8a7635e2415df550360ff70527666568500b2
hugopoggetti <hugo.poggetti@epitech.eu>
2025-12-10 10:26:56 +0100

    [Feat] shared components between client/server

---
+++ shared/components/HitBox.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** position component
+*/
+
+#ifndef COMP_HITBOX_HPP
+#define COMP_HITBOX_HPP
+
+struct HitBox {
+    float width;
+    float height;
+};
+
+#endif /* COMP_HITBOX_HPP */
