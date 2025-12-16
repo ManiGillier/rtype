227cb8f5579e2bb5ea71daa9426d750c98377d8e
Mani Gillier <mani.gillier@epitech.eu>
2025-12-16 12:21:17 +0100

    [Fix] PositionUpdatePacket getters where swapped !

--- network/packets/impl/PositionUpdatePacket.hpp
+++ network/packets/impl/PositionUpdatePacket.hpp
@@ -50,8 +50,8 @@ public:
     }
 
     auto getEntityId() const -> std::size_t { return this->id; }
-    auto getYPos() const -> float { return this->x; }
-    auto getXPos() const -> float { return this->y; }
+    auto getXPos() const -> float { return this->x; }
+    auto getYPos() const -> float { return this->y; }
 private:
     std::size_t id;
     float x;
