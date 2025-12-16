5cf1b37a5c9614c87617f0a6ac1cbdab13ee0b5b
Mani Gillier <mani.gillier@epitech.eu>
2025-12-16 11:17:37 +0100

    [Fix] Render background in background
    Not foreground you moron

--- client/states/game/gui.cpp
+++ client/states/game/gui.cpp
@@ -18,12 +18,12 @@ InGameStateGui::InGameStateGui(IGameState &gm) : gameState(gm),
 
     this->textureManager.registerTexture("client/assets/background.jpg");
 
-    r.add_render_system<Position, HitBox, ElementColor>(renderSquare);
-    r.add_render_system<Laser, Dependence, ElementColor>(renderLaser);
     r.add_render_system<HorizontalTiling, TextureComp>
         (animateTiling, std::ref(this->textureManager));
     r.add_render_system<HorizontalTiling, TextureComp>
         (renderHTiledTexture, std::ref(this->textureManager));
+    r.add_render_system<Position, HitBox, ElementColor>(renderSquare);
+    r.add_render_system<Laser, Dependence, ElementColor>(renderLaser);
 
     r.add_component<HorizontalTiling>(background, {2, 0, -50});
     r.add_component<TextureComp>(background, {"client/assets/background.jpg"});
