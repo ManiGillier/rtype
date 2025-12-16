1e8bed6984a560785afe23342e6e789a605e9166
Mani Gillier <mani.gillier@epitech.eu>
2025-12-16 18:55:07 +0100

    [Feat] Added lobby text

--- client/states/lobby/gui.cpp
+++ client/states/lobby/gui.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "gui.hpp"
+#include <raylib.h>
 
 LobbyStateGui::LobbyStateGui(IGameState &gs) : gameState(gs)
 {
@@ -13,5 +14,10 @@ LobbyStateGui::LobbyStateGui(IGameState &gs) : gameState(gs)
 
 auto LobbyStateGui::render(Registry &r) -> void
 {
+    const char *txt = "When ready, press <SPACE>";
+    const int fontSize = 40;
+    const int width = MeasureText(txt, fontSize);
+
+    DrawText(txt, GetRenderWidth() / 2 - width / 2, 50, fontSize, WHITE);
     r.render();
 }
