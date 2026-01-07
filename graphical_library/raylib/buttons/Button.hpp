6192cdd2dfb457d9953cf907ae7c53b0611cbf4b
Boo-cher <boo-cher@boo-cher.com>
2026-01-07 16:25:22 +0100

    [Update] Button.hpp

--- graphical_library/raylib/buttons/Button.hpp
+++ graphical_library/raylib/buttons/Button.hpp
@@ -12,16 +12,16 @@ namespace bt {
     class Button {
     public:
         Button(int x, int y, int width, int height, int transparency = 255);
-        virtual auto Draw() const -> void = 0;
-        virtual auto Update() -> void = 0;
-        virtual auto IsClicked() const -> bool = 0;
-        virtual auto IsHovered() const -> bool = 0;
-        virtual auto SetPosition(int x, int y) -> void = 0;
-        virtual auto SetSize(int width, int height) -> void = 0;
-        virtual auto SetTransparency(int transparency) -> void = 0;
-        virtual auto SetColor(Color color) -> void = 0;
-        virtual auto SetText(string str) -> void = 0;
-        virtual auto OnClick() -> void = 0;
+        auto Draw() const -> void = 0;
+        auto Update() -> void = 0;
+        auto IsClicked() const -> bool = 0;
+        auto IsHovered() const -> bool = 0;
+        auto SetPosition(int x, int y) -> void = 0;
+        auto SetSize(int width, int height) -> void = 0;
+        auto SetTransparency(int transparency) -> void = 0;
+        auto SetColor(Color color) -> void = 0;
+        auto SetText(string str) -> void = 0;
+        auto OnClick() -> void = 0;
     private:
         bool hovered;
         int posX;
