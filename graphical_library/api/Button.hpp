ea169d908edd20570ab38037d0bc04e8e3140c4d
Boo-cher <boo-cher@boo-cher.com>
2026-01-09 16:42:39 +0100

    [Update] Button

--- graphical_library/api/Button.hpp
+++ graphical_library/api/Button.hpp
@@ -11,18 +11,19 @@
 namespace bt {
     class Button {
     public:
-        Button(int x, int y, int width, int height, int transparency = 255);
+        MyButton(int x, int y, int width, int height, Color buttonColor,
+            Color hoveredButtonColor, int transparency = 255);
         virtual ~Button() = 0;
         virtual auto ~Button() = 0;
         virtual auto Draw() const -> void = 0;
-        virtual auto IsClicked() const -> bool = 0;
-        virtual auto IsHovered() const -> bool = 0;
+        virtual auto IsClicked() -> bool = 0;
+        virtual auto IsHovered() -> bool = 0;
         virtual auto SetPosition(int x, int y) -> void = 0;
         virtual auto SetSize(int width, int height) -> void = 0;
         virtual auto SetTransparency(int transparency) -> void = 0;
         virtual auto SetColor(Color color) -> void = 0;
-        virtual auto SetText(string str) -> void = 0;
-        virtual auto OnClick() -> void = 0;
+        virtual auto SetText(std::string str) -> void = 0;
+        virtual auto SwitchClick() -> void = 0;
     };
 }
 #endif // BUTTON_HPP_
