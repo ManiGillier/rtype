2639411a3f012ec04553298c4d00614475293ba4
Boo-cher <boo-cher@boo-cher.com>
2026-01-10 12:57:57 +0100

    Revert "[Update] normal button"
    This reverts commit 0295b331d1811d3be5310c2e3aa70d7a7607ed0c.

--- graphical_library/raylib/buttons/Button.hpp
+++ graphical_library/raylib/buttons/Button.hpp
@@ -8,27 +8,29 @@
 #ifndef BUTTON_HPP
 #define BUTTON_HPP
 
-class Button : bt::Button
-{
-public:
-    Button(int x, int y, int width, int height, int transparency = 255);
-    auto Draw() const -> void = 0;
-    auto IsClicked() const -> bool = 0;
-    auto IsHovered() const -> bool = 0;
-    auto SetPosition(int x, int y) -> void = 0;
-    auto SetSize(int width, int height) -> void = 0;
-    auto SetTransparency(int transparency) -> void = 0;
-    auto SetColor(Color color) -> void = 0;
-    auto SetText(string str) -> void = 0;
-    auto OnClick() -> void = 0;
-private:
-    bool hovered;
-    int posX;
-    int posY;
-    int sizeX;
-    int sizeY;
-    int transparency;
-    Color color;
-    string text;
-};
+namespace bt {
+    class Button {
+    public:
+        Button(int x, int y, int width, int height, int transparency = 255);
+        auto Draw() const -> void = 0;
+        auto Update() -> void = 0;
+        auto IsClicked() const -> bool = 0;
+        auto IsHovered() const -> bool = 0;
+        auto SetPosition(int x, int y) -> void = 0;
+        auto SetSize(int width, int height) -> void = 0;
+        auto SetTransparency(int transparency) -> void = 0;
+        auto SetColor(Color color) -> void = 0;
+        auto SetText(string str) -> void = 0;
+        auto OnClick() -> void = 0;
+    private:
+        bool hovered;
+        int posX;
+        int posY;
+        int sizeX;
+        int sizeY;
+        int transparency;
+        Color color;
+        string text;
+    };
+}
 #endif // BUTTON_HPP_
