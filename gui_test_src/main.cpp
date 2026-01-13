ff399a82cfeec48bba5a6cfc4c7aad2923dae9dd
Boo-cher <boo-cher@boo-cher.com>
2026-01-13 11:26:08 +0100

    [Update] test main

--- gui_test_src/main.cpp
+++ gui_test_src/main.cpp
@@ -8,29 +8,29 @@ int main()
 {
     Renderer window;
     Color badMoche = {74, 65, 42, 255};
-    window.AddButton(0, 0, 120, 60, 255, BLUE, "boo-ton");
-    window.GetButton(0).SetHoveredColor((Color){ 173, 42, 201, 255 });
-    window.AddCheckBox(false, 150, 10, 40, 255, GREEN, "");
-    window.GetCheckBox(0).SetText("const std::string &str");
-    window.AddTextBox(400, 250, 60, 60, 255, badMoche, "");
-    window.GetTextBox(0).SetText("12");
-    window.GetTextBox(0).SetHoveredColor((Color){ 255, 255, 255, 255 });
-    window.GetCheckBox(0).SetHoveredColor((Color){ 120, 12, 12, 255 });
-    window.GetCheckBox(0).SetColor(GREEN);
-    window.AddInputBox(50, 400, 600, 60, 12, RAYWHITE, "");
-    window.AddButton(650, 400, 60, 60, 255, RAYWHITE, "OK");
+    window.addButton(0, 0, 120, 60, 255, BLUE, "boo-ton");
+    window.getButton(0).setHoveredColor((Color){ 173, 42, 201, 255 });
+    window.addCheckBox(false, 150, 10, 40, 255, GREEN, "");
+    window.getCheckBox(0).setText("const std::string &str");
+    window.addTextBox(400, 250, 60, 60, 255, badMoche, "");
+    window.getTextBox(0).setText("12");
+    window.getTextBox(0).setHoveredColor({ 255, 255, 255, 255 });
+    window.getCheckBox(0).setHoveredColor({ 120, 12, 12, 255 });
+    window.getCheckBox(0).setColor(GREEN);
+    window.addInputBox(50, 400, 600, 60, 12, RAYWHITE, "");
+    window.addButton(650, 400, 60, 60, 255, RAYWHITE, "OK");
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        window.Update();
-        if (window.GetButton(0).IsClicked())
+        window.update();
+        if (window.getButton(0).isClicked())
             std::cout << "quoi ?" << std::endl;
-        if (window.GetCheckBox(0).IsChecked()) {
+        if (window.getCheckBox(0).isChecked()) {
             std::cout << "FEUR 😂😂😂😂😂😂😂" << std::endl;
         }
-        if (window.GetButton(1).IsClicked())
-            std::cout << window.GetInputBox(0).GetText() << std::endl;
+        if (window.getButton(1).isClicked())
+            std::cout << window.getInputBox(0).getText() << std::endl;
 
         EndDrawing();
     }
