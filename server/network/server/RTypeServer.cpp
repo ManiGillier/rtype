c8fbe4f7440e3f227390ff19ad9a661683bfc349
KaitoMomota <spam.clash.iyad@gmail.com>
2026-01-16 23:23:35 +0100

    [🔨] Added onClientDisconnect call

--- server/network/server/RTypeServer.cpp
+++ server/network/server/RTypeServer.cpp
@@ -67,9 +67,13 @@ void RTypeServer::onClientDisconnect(std::shared_ptr<IPollable> client)
     auto player = std::static_pointer_cast<Player>(client);
     LOG("Player disconnected to fd= " << client->getFileDescriptor());
 
-    bool hasEntityId = player->getEntityId().has_value();
     auto lobbyId = player->getLobbyId();
-
+    
+    // Remove player first (this handles entity cleanup)
+    this->_lobbyManager.removePlayer(player);
+    
+    // THEN notify other players
+    bool hasEntityId = player->getEntityId().has_value();
     if (hasEntityId) {
         auto lobby = this->_lobbyManager.getLobby(lobbyId);
         if (lobby) {
@@ -78,16 +82,13 @@ void RTypeServer::onClientDisconnect(std::shared_ptr<IPollable> client)
             auto playerDisconnect = create_packet(DestroyEntityPacket, toDestroy);
 
             auto &playersMutex = lobby->getPlayersMutex();
-            {
-                std::lock_guard<std::mutex> lock(playersMutex);
-                auto &players = lobby->getPlayers();
-                for (auto &it : players) {
-                    it->sendPacket(playerDisconnect);
-                }
+            std::lock_guard<std::mutex> lock(playersMutex);
+            auto &players = lobby->getPlayers();
+            for (auto &it : players) {
+                it->sendPacket(playerDisconnect);
             }
         }
     }
-    this->_lobbyManager.removePlayer(player);
 }
 
 bool RTypeServer::isConnected(const std::string &username)
