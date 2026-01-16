4523b92bbe8afb18708731fa1c40ae31dce28b51
Boo-cher <boo-cher@boo-cher.com>
2026-01-16 21:49:51 +0100

    [Update] Fixed text placement

--- graphical_library/raylib/buttons/Button.cpp
+++ graphical_library/raylib/buttons/Button.cpp
@@ -27,7 +27,7 @@ void Button::draw() const {
     DrawText(
         this->text.c_str(),
         this->x + 10,
-        this->y + (this->y / 2) - 10,
+        this->y + (this->height / 2) - 10,
         20,
         raylib::BLACK
     );
