e5dae9c7dca50f33acad079251fd6b24084d01fa
Mani Gillier <mani.gillier@epitech.eu>
2026-01-18 16:31:59 +0100

    [Fix] Rendering animated sprites

--- graphical_library/raylib/Raylib.cpp
+++ graphical_library/raylib/Raylib.cpp
@@ -298,7 +298,8 @@ auto Raylib::draw(gl::AnimatedSprite &sprite) -> void
     float dt = this->getDeltaTime();
     raylib::Texture rtext = this->textures[sprite.name].second;
     gl::Texture gltext = this->textures[sprite.name].first;
-    gltext.scale = (float) sprite.finalHeight / (float) gltext.size.y;
+    gltext.scale = (float) sprite.finalHeight / (float) gltext.size.y
+        * (float) sprite.verticalChoices;
 
     sprite.timeUntilNextStep -= dt;
     if (sprite.timeUntilNextStep <= 0) {
